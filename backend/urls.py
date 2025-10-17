b3cc86d89c6a46a952a883369d1538c066eed280
Таня <tania.flyora@gmail.com>
2025-10-17 16:24:21 +0300

    fix urls.video

--- backend/urls.py
+++ backend/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
 
     path('', include('backend.users.urls')),  # твої маршрути користувачів
     path('api/file/', include('backend.documents.urls')),  # додай, якщо в тебе app documents
-    path('api/video/', include('backend.video.urls')),
+    path('api/', include('backend.video.urls')),
 #    path('api/', include('backend.contact.urls')),
     path('', include('backend.order.urls')),  # додай, якщо в тебе app orders
     path('complaints/', include('backend.complaints.urls')),  # додай, якщо в тебе app orders
