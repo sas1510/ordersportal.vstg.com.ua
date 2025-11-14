7fc64bfc5c00dd939539d2d9d4d7973a17cb3026
Tania Flora <tania.flora@gmail.com>
2025-11-14 14:06:36 +0000

    .app

--- backend/portal_media/apps.py
+++ backend/portal_media/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PortalMediaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'portal_media'
+    name = 'backend.portal_media'
