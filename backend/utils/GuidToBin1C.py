6edc22d52445b62c7f6883a46e387983d816deb6
Floratania <tania.flyora@gmail.com>
2025-12-23 13:11:09 +0000

    fix form

--- backend/utils/GuidToBin1C.py
+++ backend/utils/GuidToBin1C.py
@@ -2,18 +2,17 @@ def guid_to_1c_bin(guid_str: str) -> bytes | None:
     if not guid_str:
         return None
 
-    # remove hyphens
     hex_str = guid_str.replace("-", "").lower()
 
     if len(hex_str) != 32:
         raise ValueError("Invalid GUID length")
 
-    # extract blocks
-    A = hex_str[0:8]    # first 8 chars
-    B = hex_str[8:12]   # next 4
-    C = hex_str[12:16]  # next 4
-    D = hex_str[16:20]  # next 4
-    E = hex_str[20:32]  # last 12
+
+    A = hex_str[0:8]    
+    B = hex_str[8:12]  
+    C = hex_str[12:16]  
+    D = hex_str[16:20]  
+    E = hex_str[20:32] 
 
     # correct 1C binary order:
     # [D][E][C][B][A]
@@ -44,11 +43,11 @@ def guid_to_1c_bin_2(guid: Union[str, uuid.UUID]) -> bytes | None:
         raise ValueError(f"Invalid GUID length: {hex_str}")
 
     # extract blocks
-    A = hex_str[0:8]    # 8
-    B = hex_str[8:12]   # 4
-    C = hex_str[12:16]  # 4
-    D = hex_str[16:20]  # 4
-    E = hex_str[20:32]  # 12
+    A = hex_str[0:8]    
+    B = hex_str[8:12]   
+    C = hex_str[12:16]  
+    D = hex_str[16:20]  
+    E = hex_str[20:32]  
 
     # 🔹 ТВОЄ правило (як ти просила):
     # [D][E][C][B][A]
