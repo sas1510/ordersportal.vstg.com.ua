db82736e4efee00bce8cbb31bd8673ce3c67f9d7
Floratania <tania.flyora@gmail.com>
2026-03-19 14:28:37 +0000

    delete vapid

--- backend/settings.py
+++ backend/settings.py
@@ -260,8 +260,8 @@ CHANNEL_LAYERS = {
 
 
 WEBPUSH_SETTINGS = {
-    "VAPID_PUBLIC_KEY": 'BL9wQus0T21rgqRFcjZhUmiZ6w0nv5siH9AFJBLayIqPbNCNMbsWPfjpNIQ3PH1RtnUuzbA7uGEJfwdgnmpImLY',
-    "VAPID_PRIVATE_KEY": "4eVLdiohXE3VyA0F6CqwUugCBkxPmiiT902NL3aR-4w",
+    "VAPID_PUBLIC_KEY": 'X',
+    "VAPID_PRIVATE_KEY": "X",
     "VAPID_ADMIN_EMAIL": "workvs.market@gmail.com"
 }
 
