e6635a66aecc078eb643c4d1f8793e4a4454e500
Floratania <tania.flyora@gmail.com>
2026-03-27 12:03:29 +0000

    fixed admin media

--- backend/portal_media/admin.py
+++ backend/portal_media/admin.py
@@ -1,51 +1,64 @@
+from django import forms
 from django.contrib import admin
 from django.utils.html import format_html
 from .models import MediaResource
 
+class MediaResourceForm(forms.ModelForm):
+    # Додаємо тимчасове поле для вибору файлу з комп'ютера
+    upload_file = forms.FileField(
+        required=False, 
+        label="Завантажити файл",
+        help_text="Оберіть файл, який буде збережено як бінарні дані в БД"
+    )
+
+    class Meta:
+        model = MediaResource
+        fields = '__all__'
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        uploaded_file = self.cleaned_data.get('upload_file')
+        
+        # Якщо файл завантажено, перетворюємо його в бінарні дані
+        if uploaded_file:
+            instance.file_data = uploaded_file.read()
+            # Автоматично підставляємо розширення, якщо воно не вказане
+            if not instance.file_extension:
+                instance.file_extension = uploaded_file.name.split('.')[-1]
+        
+        if commit:
+            instance.save()
+        return instance
+
 @admin.register(MediaResource)
 class MediaResourceAdmin(admin.ModelAdmin):
-    # Відображення списку ресурсів
-    list_display = ('title', 'resource_type', 'author', 'created_at', 'get_file_status')
-    list_filter = ('resource_type', 'created_at', 'author')
-    search_fields = ('title', 'description', 'url')
-    readonly_fields = ('created_at',)
+    form = MediaResourceForm
+    list_display = ('title', 'resource_type', 'author', 'created_at', 'get_file_info')
+    list_filter = ('resource_type', 'created_at')
+    
+    # Додаємо file_data у readonly_fields, щоб уникнути FieldError
+    readonly_fields = ('created_at', 'file_data')
 
-    # Розподіл полів на логічні блоки
     fieldsets = (
         ('Загальна інформація', {
             'fields': ('title', 'description', 'author', 'resource_type')
         }),
         ('Відео ресурси (YouTube/TikTok)', {
             'fields': ('url',),
-            'description': "Заповнюйте це поле тільки для відео-ресурсів."
         }),
-        ('Файлові ресурси (Binary)', {
-            'fields': ('file_extension', 'file_data'),
-            'description': "Ці поля використовуються лише для завантаження файлів безпосередньо в БД."
+        ('Файлові ресурси (БД)', {
+            'fields': ('upload_file', 'file_extension', 'file_data'),
         }),
         ('Системні дані', {
             'fields': ('created_at',),
         }),
     )
 
-    def get_file_status(self, obj):
-        """Відображає статус файлу або посилання у списку."""
-        if obj.resource_type == MediaResource.ResourceType.FILE:
-            if obj.file_data:
-                # Розрахунок приблизного розміру в КБ
-                size = len(obj.file_data) / 1024
-                return format_html('<span style="color: green;">💾 Файл ({:.1f} KB)</span>', size)
-            return format_html('<span style="color: red;">❌ Порожньо</span>')
-        return format_html('<a href="{0}" target="_blank">🔗 Перейти за посиланням</a>', obj.url)
-    
-    get_file_status.short_description = "Вміст/Статус"
-
-    def save_model(self, request, obj, form, change):
-        """Автоматично призначає автора, якщо він не вказаний."""
-        if not obj.pk and not obj.author:
-            obj.author = request.user
-        super().save_model(request, obj, form, change)
-
-    # Якщо ви хочете додати валідацію clean() безпосередньо в форму адмінки
-    def save_related(self, request, form, formsets, change):
-        super().save_related(request, form, formsets, change)
+    def get_file_info(self, obj):
+        if obj.resource_type == MediaResource.ResourceType.FILE and obj.file_data:
+            size = len(obj.file_data) / 1024
+            return format_html("💾 Файл ({:.1f} KB)", size)
+        elif obj.url:
+            return format_html('<a href="{}" target="_blank">🔗 Посилання</a>', obj.url)
+        return "-"
+    get_file_info.short_description = "Статус/Вміст"
