413f54a46856043457d7adf3cd1755d59216b668
Floratania <tania.flyora@gmail.com>
2026-04-29 06:13:43 +0000

    safari bode safe-area

--- frontend/src/components/layout/PublicLayout.jsx
+++ frontend/src/components/layout/PublicLayout.jsx
@@ -20,26 +20,33 @@ export default function PublicLayout() {
     </div>
   );
 }
-
 const styles = {
-  layout: {
+   layout: {
     display: "flex",
     flexDirection: "column",
-    minHeight: "100vh",
-    position: "relative", // Важливо для абсолютного позиціонування всередині
+    height: "100dvh",
+    boxSizing: "border-box",
+    position: "relative",
+    overflow: "hidden",
+    paddingTop: "env(safe-area-inset-top, 0px)", 
   },
   headerWrapper: {
-    position: "absolute", // Хедер "літає" над контентом
-    top: 0,
+    position: "absolute", 
+    top: "env(safe-area-inset-top, 0px)",
     left: 0,
     width: "100%",
-    zIndex: 100, // Щоб бути поверх усього
+    zIndex: 100, 
+    // paddingTop: "env(safe-area-inset-top, 0px)",
+    // paddingBottom: "env(safe-area-inset-bottom, 0px)",
   },
   main: {
     flexGrow: 1,
-    display: "flex",
-    flexDirection: "column",
-    minHeight: "100vh", // Займає рівно весь екран без дірок
+    // display: "flex",
+    // flexDirection: "column",
+    height: "100%", 
     width: "100%",
+    overflowY: "auto",
+    boxSizing: "border-box",
+    // paddingTop: "calc(48px + env(safe-area-inset-top, 0px))", // Враховуємо висоту хедера + безпечну зону
   },
 };
