420909cf4a2680efa2eca139f3d88571d7ee39fa
Floratania <tania.flyora@gmail.com>
2026-05-14 12:24:37 +0000

    Update

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -1,17 +1,23 @@
 /* eslint-env serviceworker */
 /* eslint-disable no-restricted-globals */
 self.addEventListener('push', function(event) {
-    console.log('[Service Worker] Сигнал отримано!');
+    if (process.env.NODE_ENV === "development") {
+        console.log('[Service Worker] Сигнал отримано!');
+    }
     
     let payload = {};
 
     if (event.data) {
         try {
-            // Отримуємо JSON
+ 
             payload = event.data.json();
-            console.log('[Service Worker] Дані отримано:', payload);
+            if (process.env.NODE_ENV === "development") {
+                if (process.env.NODE_ENV === "development") {
+                    console.log('[Service Worker] Дані отримано:', payload);
+                }
+            }
         } catch  {
-            // Якщо прийшов звичайний текст
+
             payload = {
                 head: 'Нове повідомлення',
                 body: event.data.text()
@@ -19,18 +25,17 @@ self.addEventListener('push', function(event) {
         }
     }
 
-    // Використовуємо дані з бекенду (з вашого Python payload)
+
     const title = payload.head || payload.title || 'Orders Portal';
     const body = payload.body || 'У вас нове сповіщення';
-    
-    // ПРІОРИТЕТ: спочатку беремо icon/url з сервера, якщо немає — ставимо дефолт
+
     const options = {
         body: body,
         icon: payload.icon || '/header_logo_small.svg', 
         badge: payload.badge || '/header_logo_small.svg',
         vibrate: [100, 50, 100],
         data: {
-            // Отримуємо URL з бекенду (http://172.17.19.107/dashboard)
+            
             url: payload.url || '/dashboard' 
         }
     };
@@ -43,18 +48,18 @@ self.addEventListener('push', function(event) {
 self.addEventListener('notificationclick', function(event) {
     event.notification.close();
     
-    // Отримуємо URL, який ми поклали в data вище
+  
     const targetUrl = event.notification.data.url;
 
     event.waitUntil(
         self.clients.matchAll({ type: 'window' }).then(windowClients => {
-            // Якщо вкладка вже відкрита — фокусуємося на ній
+        
             for (let client of windowClients) {
                 if (client.url === targetUrl && 'focus' in client) {
                     return client.focus();
                 }
             }
-            // Якщо ні — відкриваємо нову
+     
             if (self.clients.openWindow) {
                 return self.clients.openWindow(targetUrl);
             }
